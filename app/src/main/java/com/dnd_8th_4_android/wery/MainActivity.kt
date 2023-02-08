3baf8214ccae0eebfeb3287054b1f55159071496
stopkite <stopkite98@naver.com>
2023-02-08 17:25:24 +0900

    first commit

---
+++ app/src/main/java/com/dnd_8th_4_android/wery/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.dnd_8th_4_android.wery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
