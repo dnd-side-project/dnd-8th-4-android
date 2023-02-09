16586a412747cc4ef4490a166da32151d96b65a1
JiYeon Jeong <62979643+stopkite@users.noreply.github.com>
2023-02-09 21:34:08 +0900

    Revert "#6 [스플래쉬] 이동 로직 구현"

--- app/src/main/java/com/dnd_8th_4_android/wery/WeRyApplication.kt
+++
@@ -1,9 +0,0 @@
-package com.dnd_8th_4_android.wery
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class WeRyApplication:Application() {
-
-}
--- app/src/main/java/com/dnd_8th_4_android/wery/domain/model/SplashType.kt
+++
@@ -1,5 +0,0 @@
-package com.dnd_8th_4_android.wery.domain.model
-
-enum class SplashType {
-    LOGIN, ONBOARD, HOME
-}
