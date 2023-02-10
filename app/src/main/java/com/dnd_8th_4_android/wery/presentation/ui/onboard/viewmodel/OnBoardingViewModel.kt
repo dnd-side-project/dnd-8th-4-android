87c6a9cf9497fbbef33c6318cb5a95c5dc55ca02
stopkite <stopkite98@naver.com>
2023-02-11 01:42:45 +0900

    #15 [refactor] 온보딩 뷰모델 주석 제거

--- app/src/main/java/com/dnd_8th_4_android/wery/presentation/ui/onboard/viewmodel/OnBoardingViewModel.kt
+++ app/src/main/java/com/dnd_8th_4_android/wery/presentation/ui/onboard/viewmodel/OnBoardingViewModel.kt
@@ -10,16 +10,13 @@ import javax.inject.Inject
 @HiltViewModel
 class OnBoardingViewModel @Inject constructor(private val localDataSource: AuthLocalDataSource) : ViewModel() {
 
-    // indicator 현재 위치
     private var _currentPosition = MutableLiveData<Int>()
     val currentPosition: LiveData<Int> = _currentPosition
 
-    // 현재 위치 설정
     fun setCurrentPos(posValue: Int) {
         _currentPosition.value = posValue
     }
 
-    // 온보딩 상태 저장
     fun saveOnBoardingState() {
         localDataSource.hasOnboardDone = true
     }
