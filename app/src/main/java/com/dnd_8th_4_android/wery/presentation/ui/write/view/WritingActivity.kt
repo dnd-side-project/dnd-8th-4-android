0dacabf59b393600ef22730ddd54b5e399ef7cc7
stopkite <stopkite98@naver.com>
2023-02-12 20:30:38 +0900

    #26 [layout] 글쓰기 기본 레이아웃 배치

---
+++ app/src/main/java/com/dnd_8th_4_android/wery/presentation/ui/write/view/WritingActivity.kt
@@ -0,0 +1,9 @@
+package com.dnd_8th_4_android.wery.presentation.ui.write.view
+
+import com.dnd_8th_4_android.wery.R
+import com.dnd_8th_4_android.wery.databinding.ActivityWritingBinding
+import com.dnd_8th_4_android.wery.presentation.ui.base.BaseActivity
+
+class WritingActivity : BaseActivity<ActivityWritingBinding>(R.layout.activity_writing) {
+
+}
