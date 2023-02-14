b8679b47382be6f846de14f7084cd0252abc9131
wlsdn9297 <wlsdn9297@naver.com>
2023-02-15 00:15:43 +0900

    #33 [feat] 이메일 유효성 검사 추가

--- app/src/main/java/com/dnd_8th_4_android/wery/presentation/ui/sign/view/SignUpEmailFragment.kt
+++ app/src/main/java/com/dnd_8th_4_android/wery/presentation/ui/sign/view/SignUpEmailFragment.kt
@@ -1,5 +1,8 @@
 package com.dnd_8th_4_android.wery.presentation.ui.sign.view
 
+import android.util.Patterns
+import androidx.core.view.isVisible
+import androidx.core.widget.doBeforeTextChanged
 import androidx.navigation.fragment.findNavController
 import com.dnd_8th_4_android.wery.R
 import com.dnd_8th_4_android.wery.databinding.FragmentSignUpEmailBinding
@@ -7,6 +10,7 @@ import com.dnd_8th_4_android.wery.presentation.ui.base.BaseFragment
 
 class SignUpEmailFragment :
     BaseFragment<FragmentSignUpEmailBinding>(R.layout.fragment_sign_up_email) {
+
     override fun initStartView() {
 
     }
@@ -16,7 +20,25 @@ class SignUpEmailFragment :
     }
 
     override fun initAfterBinding() {
-        binding.btnNext.setOnClickListener { goToSignUpPassword() }
+        binding.etEmail.requestFocus()
+        binding.etEmail.setOnClickListener {
+            val emailText = binding.etEmail.text.toString()
+            binding.btnNext.isEnabled = emailText != ""
+        }
+
+        binding.etEmail.doBeforeTextChanged { text, _, _, _ ->
+            val emailText = text.toString()
+            binding.btnNext.isEnabled = emailText != ""
+        }
+
+        binding.btnNext.setOnClickListener {
+            val email = binding.etEmail.text.toString()
+            if (Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                goToSignUpPassword()
+            } else {
+                binding.tvEmailError.isVisible = true
+            }
+        }
     }
 
     private fun goToSignUpPassword() {
