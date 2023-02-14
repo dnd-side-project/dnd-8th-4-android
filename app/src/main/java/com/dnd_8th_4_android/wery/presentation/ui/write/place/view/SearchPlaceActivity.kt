ced68adf39bd04a28ad63cf0a995394b5fdb3730
stopkite <stopkite98@naver.com>
2023-02-14 13:03:31 +0900

    #30 [feat] 장소 어댑터 생성

--- app/src/main/java/com/dnd_8th_4_android/wery/presentation/ui/write/place/view/SearchPlaceActivity.kt
+++ app/src/main/java/com/dnd_8th_4_android/wery/presentation/ui/write/place/view/SearchPlaceActivity.kt
@@ -6,17 +6,28 @@ import androidx.core.widget.addTextChangedListener
 import com.dnd_8th_4_android.wery.R
 import com.dnd_8th_4_android.wery.databinding.ActivitySearchPlaceBinding
 import com.dnd_8th_4_android.wery.presentation.ui.base.BaseActivity
+import com.dnd_8th_4_android.wery.presentation.ui.write.place.view.adapter.SearchAdapter
 
 class SearchPlaceActivity :
     BaseActivity<ActivitySearchPlaceBinding>(R.layout.activity_search_place) {
+
+    private lateinit var searchAdapter: SearchAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         initStartView()
+        initAfterBinding()
     }
 
     private fun initStartView() {
+        searchAdapter = SearchAdapter()
+    }
+
+    private fun initDataBinding() {
+    }
+
+    private fun initAfterBinding() {
         binding.etvSearch.addTextChangedListener {
             if (it!!.isNotEmpty()) {
                 binding.viewFocused.isSelected = true
@@ -29,11 +40,5 @@ class SearchPlaceActivity :
         binding.ivSearchDelete.setOnClickListener {
             binding.etvSearch.text.clear()
         }
-    }
-
-    private fun initDataBinding() {
-    }
-
-    private fun initAfterBinding() {
     }
 }
