7cc8216b8889cc55f6048d685db7232faa4d94c8
wlsdn9297 <wlsdn9297@naver.com>
2023-02-15 11:41:53 +0900

    #33 [feat] 이메일 에러 다이얼로그 추가

--- app/src/main/java/com/dnd_8th_4_android/wery/presentation/ui/sign/view/SignUpEmailFragment.kt
+++ app/src/main/java/com/dnd_8th_4_android/wery/presentation/ui/sign/view/SignUpEmailFragment.kt
@@ -2,15 +2,14 @@ package com.dnd_8th_4_android.wery.presentation.ui.sign.view
 
 import android.util.Patterns
 import androidx.core.view.isVisible
-import androidx.core.widget.doBeforeTextChanged
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.dnd_8th_4_android.wery.R
 import com.dnd_8th_4_android.wery.databinding.FragmentSignUpEmailBinding
+import com.dnd_8th_4_android.wery.domain.model.DialogInfo
 import com.dnd_8th_4_android.wery.presentation.ui.base.BaseFragment
 import com.dnd_8th_4_android.wery.presentation.ui.sign.viewmodel.SignUpEmailViewModel
-import com.dnd_8th_4_android.wery.presentation.ui.sign.viewmodel.SignUpNameViewModel
-import java.util.regex.Pattern
+import com.dnd_8th_4_android.wery.presentation.util.DialogFragmentUtil
 
 class SignUpEmailFragment :
     BaseFragment<FragmentSignUpEmailBinding>(R.layout.fragment_sign_up_email) {
@@ -28,11 +27,29 @@ class SignUpEmailFragment :
         binding.btnNext.setOnClickListener {
             val email = binding.etEmail.text.toString()
             if (Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-                goToSignUpPassword()
+//                TODO 가입된 이메일인지 확인 기능 구현
+//                if (true) {
+                    goToSignUpPassword()
+//                } else {
+//                    showErrorDialog()
+//                }
             } else {
                 binding.tvEmailError.isVisible = true
             }
         }
+    }
+
+    private fun showErrorDialog() {
+        val dialog = DialogFragmentUtil(
+            DialogInfo(
+                null,
+                resources.getString(R.string.sign_up_email_dialog_error),
+                null,
+                resources.getString(R.string.sign_in_confirm)
+            )
+        ) {}
+
+        dialog.show(childFragmentManager, dialog.tag)
     }
 
     private fun goToSignUpPassword() {
