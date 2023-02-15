dce0d0cb0f70148fc6f348091d3909afa1be9281
wlsdn9297 <wlsdn9297@naver.com>
2023-02-15 11:27:06 +0900

    #33 [feat] 이름 레이아웃 로직 구현

--- app/src/main/java/com/dnd_8th_4_android/wery/presentation/ui/sign/view/SignUpNameFragment.kt
+++ app/src/main/java/com/dnd_8th_4_android/wery/presentation/ui/sign/view/SignUpNameFragment.kt
@@ -1,15 +1,20 @@
 package com.dnd_8th_4_android.wery.presentation.ui.sign.view
 
-import androidx.core.widget.doBeforeTextChanged
+import androidx.core.view.isVisible
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.dnd_8th_4_android.wery.R
 import com.dnd_8th_4_android.wery.databinding.FragmentSignUpNameBinding
 import com.dnd_8th_4_android.wery.presentation.ui.base.BaseFragment
+import com.dnd_8th_4_android.wery.presentation.ui.sign.viewmodel.SignUpNameViewModel
+import java.util.regex.Pattern
 
 class SignUpNameFragment :
     BaseFragment<FragmentSignUpNameBinding>(R.layout.fragment_sign_up_name) {
-    override fun initStartView() {
+    private val viewModel : SignUpNameViewModel by viewModels()
 
+    override fun initStartView() {
+        binding.vm = viewModel
     }
 
     override fun initDataBinding() {
@@ -17,14 +22,20 @@ class SignUpNameFragment :
     }
 
     override fun initAfterBinding() {
-        binding.etName.doBeforeTextChanged { _, _, _, after ->
-            binding.btnNext.isEnabled = after > 0
+        binding.btnNext.setOnClickListener {
+            val edtPassword = binding.etName.text.toString()
+            val pattern = Pattern.compile("^[가-힣]*\$")
+            val isSuccess = pattern.matcher(edtPassword).find()
+            if (isSuccess) {
+                goToSignUpEmail()
+            } else {
+                binding.tvNameError.isVisible = true
+            }
         }
-
-        binding.btnNext.setOnClickListener { goToSignUpEmail() }
     }
 
     private fun goToSignUpEmail() {
         findNavController().navigate(R.id.action_signUpNameFragment_to_signUpEmailFragment)
     }
 }
+
---
+++ app/src/main/java/com/dnd_8th_4_android/wery/presentation/ui/sign/viewmodel/SignUpNameViewModel.kt
@@ -0,0 +1,27 @@
+package com.dnd_8th_4_android.wery.presentation.ui.sign.viewmodel
+
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SignUpNameViewModel : ViewModel() {
+
+    private val _isEnabled = MutableLiveData<Boolean>()
+    val isEnabled: LiveData<Boolean> = _isEnabled
+
+    val textWatcher = object : TextWatcher {
+        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+        }
+
+        override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            _isEnabled.value = p0?.length!! > 0
+        }
+
+        override fun afterTextChanged(p0: Editable?) {
+
+        }
+    }
+}
