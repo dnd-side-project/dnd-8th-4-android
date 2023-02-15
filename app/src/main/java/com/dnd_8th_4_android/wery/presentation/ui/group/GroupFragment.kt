537a0f52569b06046464b390e34e38a4ce773a44
wlsdn9297 <wlsdn9297@naver.com>
2023-02-15 16:42:09 +0900

    #37 [feat] 그룹 메인 검색창 기능 추가

--- app/src/main/java/com/dnd_8th_4_android/wery/presentation/ui/group/GroupFragment.kt
+++ app/src/main/java/com/dnd_8th_4_android/wery/presentation/ui/group/GroupFragment.kt
@@ -1,8 +1,13 @@
 package com.dnd_8th_4_android.wery.presentation.ui.group
 
+import android.view.inputmethod.EditorInfo
+import androidx.core.view.isVisible
+import androidx.core.widget.doBeforeTextChanged
 import com.dnd_8th_4_android.wery.R
 import com.dnd_8th_4_android.wery.databinding.FragmentGroupBinding
 import com.dnd_8th_4_android.wery.presentation.ui.base.BaseFragment
+import com.dnd_8th_4_android.wery.presentation.util.hideKeyboard
+import com.dnd_8th_4_android.wery.presentation.util.showKeyboard
 
 class GroupFragment : BaseFragment<FragmentGroupBinding>(R.layout.fragment_group) {
     override fun initStartView() {
@@ -14,6 +19,23 @@ class GroupFragment : BaseFragment<FragmentGroupBinding>(R.layout.fragment_group
     }
 
     override fun initAfterBinding() {
+        binding.etSearch.doBeforeTextChanged { _, _, _, after ->
+            binding.ivSearchClose.isVisible = after > 0
+        }
 
+        binding.etSearch.setOnEditorActionListener { textView, actionId, _ ->
+            val searchKeyword = textView.text.toString()
+            if (actionId == EditorInfo.IME_ACTION_SEARCH && searchKeyword.isNotEmpty()) {
+                binding.etSearch.hideKeyboard()
+                binding.etSearch.clearFocus()
+                // TODO 검색 동작
+            }
+            false
+        }
+
+        binding.ivSearchClose.setOnClickListener {
+            binding.etSearch.text.clear()
+            binding.etSearch.showKeyboard()
+        }
     }
 }
