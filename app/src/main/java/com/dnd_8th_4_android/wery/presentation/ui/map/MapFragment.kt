2c70f95985b7bd347e9d05ec3fd892ede071a696
wlsdn9297 <wlsdn9297@naver.com>
2023-02-16 13:02:57 +0900

    [feat] 지도 레이아웃과 바텀 네비게이션 연결

---
+++ app/src/main/java/com/dnd_8th_4_android/wery/presentation/ui/map/MapFragment.kt
@@ -0,0 +1,19 @@
+package com.dnd_8th_4_android.wery.presentation.ui.map
+
+import com.dnd_8th_4_android.wery.R
+import com.dnd_8th_4_android.wery.databinding.FragmentMapBinding
+import com.dnd_8th_4_android.wery.presentation.ui.base.BaseFragment
+
+class MapFragment : BaseFragment<FragmentMapBinding>(R.layout.fragment_map) {
+    override fun initStartView() {
+
+    }
+
+    override fun initDataBinding() {
+
+    }
+
+    override fun initAfterBinding() {
+
+    }
+}
