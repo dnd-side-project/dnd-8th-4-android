ccc569bb0c24e8ced399a848943d81c2cbbe432f
stopkite <stopkite98@naver.com>
2023-02-19 21:15:09 +0900

    #49 [feat] 위치권한 설정 코드 추가

--- app/src/main/java/com/dnd_8th_4_android/wery/presentation/ui/map/MapFragment.kt
+++ app/src/main/java/com/dnd_8th_4_android/wery/presentation/ui/map/MapFragment.kt
@@ -1,12 +1,69 @@
 package com.dnd_8th_4_android.wery.presentation.ui.map
 
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
+import android.provider.Settings
+import androidx.activity.result.contract.ActivityResultContracts
 import com.dnd_8th_4_android.wery.R
 import com.dnd_8th_4_android.wery.databinding.FragmentMapBinding
+import com.dnd_8th_4_android.wery.domain.model.DialogInfo
 import com.dnd_8th_4_android.wery.presentation.ui.base.BaseFragment
+import com.dnd_8th_4_android.wery.presentation.util.DialogFragmentUtil
+import net.daum.mf.map.api.MapView
 
 class MapFragment : BaseFragment<FragmentMapBinding>(R.layout.fragment_map) {
-    override fun initStartView() {
 
+    private val locationPermissionRequest = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissions ->
+        when {
+            permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
+                initMapView()
+            }
+            permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
+                initMapView()
+            }
+            else -> {
+                // 권한 거부
+                permissionDialog()
+            }
+        }
+    }
+
+    override fun initStartView() {
+        locationPermissionRequest.launch(
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        )
+    }
+
+    private fun permissionDialog() {
+        fun doPositiveClick() {
+            startActivity(
+                Intent(
+                    Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                    Uri.fromParts("package", context?.packageName, null)
+                )
+            )
+        }
+
+        val dialog = DialogFragmentUtil(
+            DialogInfo(
+                "위치 접근 권한",
+                "위치 접근 권한이 필요합니다.\n확인을 누르면 설정화면으로 이동합니다.",
+                "닫기",
+                "확인"
+            )
+        ) { doPositiveClick() }
+        dialog.show(requireActivity().supportFragmentManager, dialog.tag)
+    }
+
+    private fun initMapView() {
+        val mapView = MapView(requireActivity())
+        binding.layoutMapView.addView(mapView)
     }
 
     override fun initDataBinding() {
