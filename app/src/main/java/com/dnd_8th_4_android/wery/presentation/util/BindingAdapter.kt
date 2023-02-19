b7524228386b0b165efa14c78eb49888dfd188a3
wlsdn9297 <wlsdn9297@naver.com>
2023-02-19 10:59:36 +0900

    [refactor] 바인딩 어댑터 합치기

--- app/src/main/java/com/dnd_8th_4_android/wery/presentation/util/BindingAdapter.kt
+++ app/src/main/java/com/dnd_8th_4_android/wery/presentation/util/BindingAdapter.kt
@@ -1,16 +1,39 @@
 package com.dnd_8th_4_android.wery.presentation.util
 
+import android.graphics.Color
 import android.widget.ImageView
+import androidx.annotation.ColorInt
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 
 object BindingAdapter {
 
     @JvmStatic
-    @androidx.databinding.BindingAdapter("imageBindFitXY")
+    @BindingAdapter("imageBindFitXY")
     fun setImageFit(imageView: ImageView, imageUrl: String?) {
         Glide.with(imageView.context)
             .load(imageUrl)
             .fitCenter()
             .into(imageView)
+    }
+
+    @JvmStatic
+    @BindingAdapter(
+        value = ["dividerHeight", "dividerPadding", "dividerColor"],
+        requireAll = false
+    )
+    fun RecyclerView.setDivider(
+        dividerHeight: Float?,
+        dividerPadding: Float?,
+        @ColorInt dividerColor: Int?,
+    ) {
+        val decoration = CustomDecoration(
+            height = dividerHeight ?: 0f,
+            padding = dividerPadding ?: 0f,
+            color = dividerColor ?: Color.TRANSPARENT
+        )
+
+        addItemDecoration(decoration)
     }
 }
