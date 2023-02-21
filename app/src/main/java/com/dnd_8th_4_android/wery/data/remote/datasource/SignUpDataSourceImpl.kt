07a2faf9c63cba0251cad6b9de8e518632d68d1e
wlsdn9297 <wlsdn9297@naver.com>
2023-02-21 14:57:08 +0900

    #61 [feat] 회원가입 서버 통신 구현

---
+++ app/src/main/java/com/dnd_8th_4_android/wery/data/remote/datasource/SignUpDataSourceImpl.kt
@@ -0,0 +1,13 @@
+package com.dnd_8th_4_android.wery.data.remote.datasource
+
+import com.dnd_8th_4_android.wery.data.api.SignUpService
+import com.dnd_8th_4_android.wery.data.remote.model.sign.RequestSignUpData
+import com.dnd_8th_4_android.wery.data.remote.model.sign.ResponseSignUpData
+import javax.inject.Inject
+
+class SignUpDataSourceImpl @Inject constructor(private val signUpService: SignUpService): SignUpDataSource {
+
+    override suspend fun signUp(body: RequestSignUpData): ResponseSignUpData {
+        return signUpService.signUp(body)
+    }
+}
--- app/src/main/java/com/dnd_8th_4_android/wery/presentation/ui/sign/viewmodel/SignUpNicknameViewModel.kt
+++ app/src/main/java/com/dnd_8th_4_android/wery/presentation/ui/sign/viewmodel/SignUpNicknameViewModel.kt
@@ -2,14 +2,23 @@ package com.dnd_8th_4_android.wery.presentation.ui.sign.viewmodel
 
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.dnd_8th_4_android.wery.data.remote.model.sign.RequestSignUpData
+import com.dnd_8th_4_android.wery.data.remote.model.sign.ResponseSignInData
+import com.dnd_8th_4_android.wery.data.remote.model.sign.ResponseSignUpData
+import com.dnd_8th_4_android.wery.domain.repository.SignUpRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
-class SignUpNicknameViewModel @Inject constructor() : ViewModel() {
+class SignUpNicknameViewModel @Inject constructor(private val signUpRepository: SignUpRepository) :
+    ViewModel() {
     val signUpNickname = MutableLiveData<String>()
 
     private val _isEnabled = MutableLiveData<Boolean>()
@@ -17,6 +26,12 @@ class SignUpNicknameViewModel @Inject constructor() : ViewModel() {
 
     private val _textCount = MutableLiveData(0)
     val textCount: LiveData<Int> = _textCount
+
+    private val _isExisted = MutableLiveData<Boolean>()
+    val isExisted: LiveData<Boolean> = _isExisted
+
+    private val _signUpSuccess = MutableLiveData<Boolean>()
+    val signUpSuccess: LiveData<Boolean> = _signUpSuccess
 
     val textWatcher = object : TextWatcher {
         override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
@@ -30,6 +45,18 @@ class SignUpNicknameViewModel @Inject constructor() : ViewModel() {
 
         override fun afterTextChanged(p0: Editable?) {
 
+        }
+    }
+
+    fun signUp(body: RequestSignUpData) {
+        viewModelScope.launch {
+            kotlin.runCatching {
+                signUpRepository.signUp(body)
+            }.onSuccess {
+                _isExisted.value = it.data == null
+            }.onFailure {
+                Timber.tag("error").d(it.message.toString())
+            }
         }
     }
 }
