5d5eb1c1dc27edc7f5633263a394b3a2b82c8c8a
stopkite <stopkite98@naver.com>
2023-02-24 18:37:17 +0900

    #81 [feat] 미션 탭 fragment 추가

--- app/src/main/java/com/dnd_8th_4_android/wery/presentation/ui/mission/MissionFragment.kt
+++ app/src/main/java/com/dnd_8th_4_android/wery/presentation/ui/mission/MissionFragment.kt
@@ -1,24 +1,42 @@
 package com.dnd_8th_4_android.wery.presentation.ui.mission
 
-import android.annotation.SuppressLint
-import android.content.Context
-import android.location.LocationManager
-import android.util.Log
-import android.widget.Toast
 import com.dnd_8th_4_android.wery.R
 import com.dnd_8th_4_android.wery.databinding.FragmentMissionBinding
 import com.dnd_8th_4_android.wery.presentation.ui.base.BaseFragment
+import com.dnd_8th_4_android.wery.presentation.ui.mission.mymission.view.MyMissionFragment
+import com.dnd_8th_4_android.wery.presentation.ui.mission.sticker.StickerFragment
+import com.google.android.material.tabs.TabLayout
 
 class MissionFragment : BaseFragment<FragmentMissionBinding>(R.layout.fragment_mission) {
 
-    override fun initStartView() {
-    }
+    override fun initStartView() {}
 
-    override fun initDataBinding() {
-
-    }
+    override fun initDataBinding() {}
 
     override fun initAfterBinding() {
+        getMyMissionTab()
+
+        binding.tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(p0: TabLayout.Tab?) {
+                if (p0?.position == 0) getMyMissionTab()
+                else getStickerTab()
+            }
+
+            override fun onTabUnselected(p0: TabLayout.Tab?) {}
+
+            override fun onTabReselected(p0: TabLayout.Tab?) {}
+        })
     }
 
+    private fun getMyMissionTab() {
+        requireActivity().supportFragmentManager.beginTransaction()
+            .replace(R.id.mission_frame_layout, MyMissionFragment())
+            .commitAllowingStateLoss()
+    }
+
+    private fun getStickerTab() {
+        requireActivity().supportFragmentManager.beginTransaction()
+            .replace(R.id.mission_frame_layout, StickerFragment())
+            .commitAllowingStateLoss()
+    }
 }
