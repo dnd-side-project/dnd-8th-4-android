b895b31904531c24ff8854f3d2943c21283be736
stopkite <stopkite98@naver.com>
2023-02-25 20:45:14 +0900

    #86 [feat] 장소 추가 기능 구현

--- app/src/main/java/com/dnd_8th_4_android/wery/presentation/ui/mission/create/CreateMissionActivity.kt
+++ app/src/main/java/com/dnd_8th_4_android/wery/presentation/ui/mission/create/CreateMissionActivity.kt
@@ -1,15 +1,19 @@
 package com.dnd_8th_4_android.wery.presentation.ui.mission.create
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.core.widget.addTextChangedListener
 import com.dnd_8th_4_android.wery.R
 import com.dnd_8th_4_android.wery.databinding.ActivityCreateMissionBinding
 import com.dnd_8th_4_android.wery.presentation.ui.base.BaseActivity
+import com.dnd_8th_4_android.wery.presentation.ui.write.place.view.SearchPlaceActivity
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayout.OnTabSelectedListener
 
@@ -17,6 +21,16 @@ class CreateMissionActivity :
     BaseActivity<ActivityCreateMissionBinding>(R.layout.activity_create_mission) {
 
     private val viewModel: CreateMissionViewModel by viewModels()
+
+    private val requestSearchActivity =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { it ->
+            if (it.resultCode == Activity.RESULT_OK) {
+                val selectedPlace = it.data?.getStringExtra("selectedPlace")
+                    ?: getString(R.string.create_mission_place_hint)
+                viewModel.setSelectedPlace(selectedPlace)
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -27,14 +41,17 @@ class CreateMissionActivity :
 
     private fun initStartView() {
         binding.vm = viewModel
-
         binding.missionTabLayout.apply {
             addTab(this.newTab().setText(resources.getString(R.string.create_mission_due_exist)))
             addTab(this.newTab().setText(resources.getString(R.string.create_mission_due_no_exist)))
         }
+        viewModel.setSelectedPlace(resources.getString(R.string.create_mission_place_hint))
     }
 
     private fun initDataBinding() {
+        viewModel.selectedPlaceTxt.observe(this) {
+            binding.tvVisitPlace.text = it
+        }
         setTxtError(binding.etvMissionName,binding.tvMissionNameLimit,20,binding.ivMissionNameClose)
         setTxtCancelListener(binding.etvMissionName,binding.ivMissionNameClose)
     }
@@ -49,6 +66,9 @@ class CreateMissionActivity :
             override fun onTabUnselected(tab: TabLayout.Tab?) {}
             override fun onTabReselected(tab: TabLayout.Tab?) {}
         })
+        binding.layoutMissionPlace.setOnClickListener {
+            requestSearchActivity.launch(Intent(this, SearchPlaceActivity::class.java))
+        }
     }
 
     private fun setTxtError(etv: EditText, tv: TextView, lenCnt: Int, ivClose: ImageView) {
--- app/src/main/java/com/dnd_8th_4_android/wery/presentation/ui/mission/create/CreateMissionViewModel.kt
+++ app/src/main/java/com/dnd_8th_4_android/wery/presentation/ui/mission/create/CreateMissionViewModel.kt
@@ -1,8 +1,16 @@
 package com.dnd_8th_4_android.wery.presentation.ui.mission.create
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class CreateMissionViewModel : ViewModel() {
     var missionNameTxt = MutableLiveData<String>()
+
+    private var _selectedPlaceTxt = MutableLiveData<String>()
+    val selectedPlaceTxt: LiveData<String> = _selectedPlaceTxt
+
+    fun setSelectedPlace(stringValue:String) {
+        _selectedPlaceTxt.value = stringValue
+    }
 }
