5a1ff3bffc4dcea5f904d59b798f4c7b2e9607c6
stopkite <stopkite98@naver.com>
2023-02-27 00:54:50 +0900

    [refactor] navigation component 코드 수정

--- app/src/main/java/com/dnd_8th_4_android/wery/presentation/ui/home/view/MainActivity.kt
+++ app/src/main/java/com/dnd_8th_4_android/wery/presentation/ui/home/view/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.dnd_8th_4_android.wery.presentation.ui.home.view
 
 import android.os.Bundle
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.dnd_8th_4_android.wery.R
 import com.dnd_8th_4_android.wery.databinding.ActivityMainBinding
 import com.dnd_8th_4_android.wery.presentation.ui.base.BaseActivity
@@ -17,9 +19,24 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     }
 
     private fun initStartView() {
-        val navHostFragment =
+        /*val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.navHostHome) as NavHostFragment
         val navController = navHostFragment.navController
-        NavigationUI.setupWithNavController(binding.bottomNavigation, navController)
+        NavigationUI.setupWithNavController(binding.bottomNavigation, navController)*/
+
+        val host: NavHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navHostHome) as NavHostFragment? ?: return
+        val navController = host.navController
+
+        setupBottomNavMenu(navController)
+    }
+
+    private fun setupBottomNavMenu(navController: NavController) {
+        val bottomNav = binding.bottomNavigation
+        bottomNav.setupWithNavController(navController)
+        bottomNav.setOnItemSelectedListener {
+            NavigationUI.onNavDestinationSelected(it, navController)
+            return@setOnItemSelectedListener true
+        }
     }
 }
