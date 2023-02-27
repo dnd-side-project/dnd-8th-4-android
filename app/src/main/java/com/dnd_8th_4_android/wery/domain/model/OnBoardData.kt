3b278201e4e6f2195859dcd6750f1d23dbd080e7
stopkite <stopkite98@naver.com>
2023-02-27 14:42:39 +0900

    #102 [layout] 온보딩 asset 추가

---
+++ app/src/main/java/com/dnd_8th_4_android/wery/domain/model/OnBoardData.kt
@@ -0,0 +1,9 @@
+package com.dnd_8th_4_android.wery.domain.model
+
+import android.graphics.drawable.Drawable
+
+data class OnBoardData(
+    val imgDrawable: Drawable,
+    val title: String,
+    val content: String
+)
--- app/src/main/java/com/dnd_8th_4_android/wery/presentation/ui/onboard/view/OnBoardingActivity.kt
+++ app/src/main/java/com/dnd_8th_4_android/wery/presentation/ui/onboard/view/OnBoardingActivity.kt
@@ -3,9 +3,11 @@ package com.dnd_8th_4_android.wery.presentation.ui.onboard.view
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.core.content.ContextCompat
 import androidx.viewpager2.widget.ViewPager2.OnPageChangeCallback
 import com.dnd_8th_4_android.wery.R
 import com.dnd_8th_4_android.wery.databinding.ActivityOnboardingBinding
+import com.dnd_8th_4_android.wery.domain.model.OnBoardData
 import com.dnd_8th_4_android.wery.presentation.ui.base.BaseActivity
 import com.dnd_8th_4_android.wery.presentation.ui.onboard.adapter.OnBoardingAdapter
 import com.dnd_8th_4_android.wery.presentation.ui.onboard.viewmodel.OnBoardingViewModel
@@ -28,8 +30,18 @@ class OnBoardingActivity : BaseActivity<ActivityOnboardingBinding>(R.layout.acti
         setOnBoardVp()
     }
 
+    private fun setOnBoardList():MutableList<OnBoardData> {
+        val onBoardList = mutableListOf<OnBoardData>()
+        onBoardList.add(OnBoardData(ContextCompat.getDrawable(this,R.drawable.img_onboard_one)!!,resources.getString(R.string.onBoard_title_one),resources.getString(R.string.onBoard_content_one)))
+        onBoardList.add(OnBoardData( ContextCompat.getDrawable(this,R.drawable.img_onboard_two)!!,resources.getString(R.string.onBoard_title_two),resources.getString(R.string.onBoard_content_two)))
+        onBoardList.add(OnBoardData( ContextCompat.getDrawable(this,R.drawable.img_onboard_three)!!,resources.getString(R.string.onBoard_title_three),resources.getString(R.string.onBoard_content_three)))
+        onBoardList.add(OnBoardData( ContextCompat.getDrawable(this,R.drawable.img_onboard_four)!!,resources.getString(R.string.onBoard_title_four),resources.getString(R.string.onBoard_content_four)))
+        return onBoardList
+    }
+
     private fun setOnBoardVp() {
         onBoardingAdapter = OnBoardingAdapter()
+        onBoardingAdapter.itemList = setOnBoardList()
         binding.vpOnboarding.adapter = onBoardingAdapter
         binding.vpIndicator.attachTo(binding.vpOnboarding)
     }
@@ -53,7 +65,8 @@ class OnBoardingActivity : BaseActivity<ActivityOnboardingBinding>(R.layout.acti
             when (onBoardingViewModel.currentPosition.value) {
                 0 -> binding.vpOnboarding.currentItem = 1
                 1 -> binding.vpOnboarding.currentItem = 2
-                2 -> {
+                2 -> binding.vpOnboarding.currentItem = 3
+                3 -> {
                     finish()
                     onBoardingViewModel.saveOnBoardingState()
                     startActivity(Intent(this, SignActivity::class.java))
