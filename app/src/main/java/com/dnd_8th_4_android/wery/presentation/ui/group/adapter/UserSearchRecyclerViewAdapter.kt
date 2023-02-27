053f779d15f293356779ebb615a94d4a75635b17
wlsdn9297 <wlsdn9297@naver.com>
2023-02-27 21:34:24 +0900

    #103 [feat] 그룹원 초대 화면 로직 구현

--- app/src/main/java/com/dnd_8th_4_android/wery/presentation/ui/group/adapter/UserSearchRecyclerViewAdapter.kt
+++ app/src/main/java/com/dnd_8th_4_android/wery/presentation/ui/group/adapter/UserSearchRecyclerViewAdapter.kt
@@ -1,11 +1,14 @@
 package com.dnd_8th_4_android.wery.presentation.ui.group.adapter
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.dnd_8th_4_android.wery.R
 import com.dnd_8th_4_android.wery.data.remote.model.group.ResponseUserSearchData
 import com.dnd_8th_4_android.wery.databinding.ItemGroupInformationBinding
 
@@ -14,8 +17,10 @@ class UserSearchRecyclerViewAdapter :
         diffUtil
     ) {
     private lateinit var binding: ItemGroupInformationBinding
+    private lateinit var onUserClickListener: OnUserClickListener
+    private var oldLayout: View? = null
 
-    class ViewHolder(private val binding: ItemGroupInformationBinding) :
+    inner class ViewHolder(private val binding: ItemGroupInformationBinding) :
         RecyclerView.ViewHolder(binding.root) {
         fun bind(item: ResponseUserSearchData.Data.UserSearchInfoList) {
             binding.ivFriendImage.clipToOutline = true
@@ -23,6 +28,18 @@ class UserSearchRecyclerViewAdapter :
                 .into(binding.ivFriendImage)
 
             binding.tvFriendName.text = item.userNickName
+
+            binding.layoutSearch.setOnClickListener {
+                if (oldLayout != null) {
+                    oldLayout!!.background =
+                        ContextCompat.getDrawable(binding.root.context, android.R.color.transparent)
+                }
+                binding.layoutSearch.background =
+                    ContextCompat.getDrawable(binding.root.context, R.color.color_1af47aff)
+
+                oldLayout = binding.layoutSearch
+                onUserClickListener.onClicked(item.userId)
+            }
         }
     }
 
@@ -34,6 +51,18 @@ class UserSearchRecyclerViewAdapter :
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.bind(currentList[position])
+    }
+
+    fun setOnUserClickListener(listener: (Int) -> Unit) {
+        onUserClickListener = object : OnUserClickListener {
+            override fun onClicked(userId: Int) {
+                listener(userId)
+            }
+        }
+    }
+
+    interface OnUserClickListener {
+        fun onClicked(userId: Int)
     }
 
     companion object {
--- app/src/main/java/com/dnd_8th_4_android/wery/presentation/ui/group/view/UserSearchActivity.kt
+++ app/src/main/java/com/dnd_8th_4_android/wery/presentation/ui/group/view/UserSearchActivity.kt
@@ -1,12 +1,19 @@
 package com.dnd_8th_4_android.wery.presentation.ui.group.view
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.inputmethod.EditorInfo
 import androidx.activity.viewModels
+import androidx.core.view.isVisible
+import androidx.core.widget.doBeforeTextChanged
 import com.dnd_8th_4_android.wery.R
 import com.dnd_8th_4_android.wery.databinding.ActivitySearchUserInviteBinding
 import com.dnd_8th_4_android.wery.presentation.ui.base.BaseActivity
 import com.dnd_8th_4_android.wery.presentation.ui.group.adapter.UserSearchRecyclerViewAdapter
 import com.dnd_8th_4_android.wery.presentation.ui.group.viewmodel.UserSearchViewModel
+import com.dnd_8th_4_android.wery.presentation.util.hideKeyboard
+import com.dnd_8th_4_android.wery.presentation.util.showKeyboard
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -29,10 +36,45 @@ class UserSearchActivity :
     }
 
     private fun initStartView() {
-        userSearchRecyclerViewAdapter = UserSearchRecyclerViewAdapter()
-        binding.rvSearch.apply {
-            adapter = userSearchRecyclerViewAdapter
-            itemAnimator = null
+        binding.etSearch.requestFocus()
+        Handler(Looper.getMainLooper())
+            .postDelayed({
+                binding.etSearch.showKeyboard()
+            }, 400)
+
+        binding.etSearch.doBeforeTextChanged { _, _, _, after ->
+            binding.ivSearchClose.isVisible = after > 0
+        }
+
+        binding.etSearch.setOnEditorActionListener { textView, actionId, _ ->
+            binding.tvInvite.isEnabled = false
+            viewModel.searchKeyword.value = textView.text.toString()
+            if (actionId == EditorInfo.IME_ACTION_SEARCH && viewModel.searchKeyword.value != "") {
+                if (!binding.rvSearch.isVisible) {
+                    binding.rvSearch.isVisible = true
+                    binding.ivSearch.isVisible = false
+                }
+                userSearchRecyclerViewAdapter = UserSearchRecyclerViewAdapter()
+                userSearchRecyclerViewAdapter.setOnUserClickListener {
+                    binding.tvInvite.isEnabled = true
+                }
+
+                binding.rvSearch.apply {
+                    adapter = userSearchRecyclerViewAdapter
+                    itemAnimator = null
+                }
+
+                viewModel.getUserSearchList()
+                binding.etSearch.hideKeyboard()
+                binding.etSearch.clearFocus()
+            }
+            false
+        }
+
+        binding.ivSearchClose.setOnClickListener {
+            binding.etSearch.text.clear()
+            binding.etSearch.requestFocus()
+            binding.etSearch.showKeyboard()
         }
     }
 }
