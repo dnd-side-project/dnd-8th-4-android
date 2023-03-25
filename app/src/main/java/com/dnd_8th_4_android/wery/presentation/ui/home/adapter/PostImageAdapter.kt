3ca3d375973b891f6b29b76412d7c3dac27b2afb
wlsdn9297 <wlsdn9297@naver.com>
2023-03-25 17:00:33 +0900

    [refactor] 메인 홈 게시글 이미지 레이아웃 리팩토링

--- app/src/main/java/com/dnd_8th_4_android/wery/presentation/ui/home/adapter/PostImageAdapter.kt
+++ app/src/main/java/com/dnd_8th_4_android/wery/presentation/ui/home/adapter/PostImageAdapter.kt
@@ -7,10 +7,12 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.dnd_8th_4_android.wery.R
 import com.dnd_8th_4_android.wery.data.remote.model.home.ResponsePostData
 import com.dnd_8th_4_android.wery.databinding.ItemPostImageBinding
 
-class PostImageAdapter : ListAdapter<ResponsePostData.Data.Content.Images, PostImageAdapter.ViewHolder>(diffUtil) {
+class PostImageAdapter :
+    ListAdapter<ResponsePostData.Data.Content.Images, PostImageAdapter.ViewHolder>(diffUtil) {
     private lateinit var binding: ItemPostImageBinding
     private lateinit var postDetailImageListener: PostDetailImageListener
 
@@ -20,11 +22,14 @@ class PostImageAdapter : ListAdapter<ResponsePostData.Data.Content.Images, PostI
             binding.ivFriendPostImage.clipToOutline = true
             Glide.with(binding.ivFriendPostImage.context).load(item.imageUrl)
                 .into(binding.ivFriendPostImage)
-            binding.tvPostImageCount.text = "${adapterPosition.inc()}"
-            binding.tvPostImageAllCount.text = currentList.size.toString()
+            binding.tvPostImageCount.text = binding.root.resources.getString(
+                R.string.home_post_image,
+                adapterPosition.inc(),
+                currentList.size
+            )
 
             if (currentList.size == 1) {
-                binding.layoutPostImageCount.isVisible = false
+                binding.tvPostImageCount.isVisible = false
             }
 
             binding.ivFriendPostImage.setOnClickListener {
@@ -55,20 +60,21 @@ class PostImageAdapter : ListAdapter<ResponsePostData.Data.Content.Images, PostI
     }
 
     companion object {
-        private val diffUtil = object : DiffUtil.ItemCallback<ResponsePostData.Data.Content.Images>() {
-            override fun areItemsTheSame(
-                oldItem: ResponsePostData.Data.Content.Images,
-                newItem: ResponsePostData.Data.Content.Images,
-            ): Boolean {
-                return oldItem == newItem
-            }
+        private val diffUtil =
+            object : DiffUtil.ItemCallback<ResponsePostData.Data.Content.Images>() {
+                override fun areItemsTheSame(
+                    oldItem: ResponsePostData.Data.Content.Images,
+                    newItem: ResponsePostData.Data.Content.Images,
+                ): Boolean {
+                    return oldItem == newItem
+                }
 
-            override fun areContentsTheSame(
-                oldItem: ResponsePostData.Data.Content.Images,
-                newItem: ResponsePostData.Data.Content.Images,
-            ): Boolean {
-                return oldItem == newItem
+                override fun areContentsTheSame(
+                    oldItem: ResponsePostData.Data.Content.Images,
+                    newItem: ResponsePostData.Data.Content.Images,
+                ): Boolean {
+                    return oldItem == newItem
+                }
             }
-        }
     }
 }
