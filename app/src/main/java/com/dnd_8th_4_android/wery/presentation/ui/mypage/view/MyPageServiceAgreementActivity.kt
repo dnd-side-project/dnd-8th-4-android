7f8d665272b5dc0853c792c7003ae377703edf0b
wlsdn9297 <wlsdn9297@naver.com>
2023-03-27 09:34:25 +0900

    [feat] 이용약관 웹 페이지 연결

--- app/src/main/java/com/dnd_8th_4_android/wery/presentation/ui/mypage/view/MyPageServiceAgreementActivity.kt
+++ app/src/main/java/com/dnd_8th_4_android/wery/presentation/ui/mypage/view/MyPageServiceAgreementActivity.kt
@@ -1,5 +1,7 @@
 package com.dnd_8th_4_android.wery.presentation.ui.mypage.view
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import com.dnd_8th_4_android.wery.R
 import com.dnd_8th_4_android.wery.databinding.ActivityMypageServiceAgreementBinding
@@ -18,6 +20,33 @@ class MyPageServiceAgreementActivity :
         binding.ivBack.setOnClickListener {
             finish()
             overridePendingTransition(0, 0)
+        }
+
+        binding.tvServiceAgreementContent.setOnClickListener {
+            startActivity(
+                Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("https://emerald-calf-c8d.notion.site/d7cdf86e96494920bfbbfd2aed297f48")
+                )
+            )
+        }
+
+        binding.tvPolicy.setOnClickListener {
+            startActivity(
+                Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("https://emerald-calf-c8d.notion.site/0c3e82a2657d4d7282116f55d3b6386e")
+                )
+            )
+        }
+
+        binding.tvPositionTerms.setOnClickListener {
+            startActivity(
+                Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("https://emerald-calf-c8d.notion.site/b61e017d283949d99be9411a5d63828b")
+                )
+            )
         }
     }
 }
--- app/src/main/java/com/dnd_8th_4_android/wery/presentation/ui/sign/view/SignUpUseAgreementFragment.kt
+++ app/src/main/java/com/dnd_8th_4_android/wery/presentation/ui/sign/view/SignUpUseAgreementFragment.kt
@@ -1,5 +1,7 @@
 package com.dnd_8th_4_android.wery.presentation.ui.sign.view
 
+import android.content.Intent
+import android.net.Uri
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.dnd_8th_4_android.wery.R
@@ -24,6 +26,33 @@ class SignUpUseAgreementFragment :
     override fun initAfterBinding() {
         binding.ivBack.setOnClickListener {
             findNavController().navigate(R.id.action_signUpUseAgreementFragment_to_signInFragment)
+        }
+
+        binding.ivServiceAgreement.setOnClickListener {
+            startActivity(
+                Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("https://emerald-calf-c8d.notion.site/d7cdf86e96494920bfbbfd2aed297f48")
+                )
+            )
+        }
+
+        binding.ivPolicy.setOnClickListener {
+            startActivity(
+                Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("https://emerald-calf-c8d.notion.site/0c3e82a2657d4d7282116f55d3b6386e")
+                )
+            )
+        }
+
+        binding.ivPositionTerms.setOnClickListener {
+            startActivity(
+                Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("https://emerald-calf-c8d.notion.site/b61e017d283949d99be9411a5d63828b")
+                )
+            )
         }
 
         binding.cbAllAgree.setOnCheckedChangeListener { _, checked ->
